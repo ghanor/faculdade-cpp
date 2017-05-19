90bcde65d3f47fa9fc2e9a28e6d1793e4df99d3c
Leonardo Almeida <leoziinhow.ferreira@gmail.com>
2017-05-19 13:08:02 -0300

    Update graus-radianos.cpp

--- graus-radianos.cpp
+++ graus-radianos.cpp
@@ -5,35 +5,49 @@ using namespace std;
 
 int main()
 {
-    char seno, cosseno, graus, radianos;
-    cin >> seno >> cosseno >> graus >> radianos;
-    float valor;
-    cin >> valor;
-    if (seno == 's' && graus == 'g')
+    char tipo;
+    cin >> tipo;
+    
+    if (tipo == 's')
     {
-        float calcular_Seno;
-        calcular_Seno = sin(valor);
-        cout << calcular_Seno << endl;
-    }
-    else if (seno == 's' && radianos == 'r')
-    {
-        float calcular_Seno;
-        calcular_Seno = asin(valor);
-        cout << calcular_Seno << endl;
-    }
-    else if (cosseno == 'c' && graus == 'g')
-    {
-        float calcular_Cosseno;
-        calcular_Cosseno = cos(valor);
-        cout << calcular_Cosseno << endl;
+        char tipo2;
+        cin >> tipo2;
+        if (tipo2 == 'g')
+        {
+            float calcular_Seno, valor;
+            cin >> valor;
+            calcular_Seno = sin(valor);
+            cout << calcular_Seno << endl;
+        }
+        else 
+        {
+            float calcular_Seno, valor;
+            cin >> valor;
+            calcular_Seno = asin(valor);
+            cout << calcular_Seno << endl;
+        }
     }
     else
     {
-        float calcular_Cosseno;
-        calcular_Cosseno = acos(valor);
-        cout << calcular_Cosseno << endl;
+        char tipo2;
+        cin >> tipo2;
+        if(tipo2 == 'g')
+        {
+            float calcular_Cosseno, valor;
+            cin >> valor;
+            calcular_Cosseno = cos(valor);
+            cout << calcular_Cosseno << endl;
+        }
+        else 
+        {
+            float calcular_Cosseno, valor;
+            cin >> valor;
+            calcular_Cosseno = acos(valor);
+            cout << calcular_Cosseno << endl;
+        }
     }
     return 0;
     
 }
+
 
