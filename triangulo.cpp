2f8e51e65805b27b33a62d0dd899cfec45ede1b8
Leonardo Almeida <leoziinhow.ferreira@gmail.com>
2017-05-19 09:18:53 -0300

    Add files via upload

---
+++ triangulo.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int lado1, lado2, lado3;
+    cin >> lado1 >> lado2 >> lado3;
+    
+    if (lado1 < (lado2 + lado3) && lado2 < (lado1 + lado3) && lado3 < (lado2 + lado1))
+        if (lado1 == lado2 && lado2 == lado3)
+        {
+            cout << "equilatero" << endl;
+        }
+        else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+        {
+            cout << "isosceles" << endl;
+        }
+        else 
+        {
+            cout << "escaleno" << endl;
+        }
+    else 
+    {
+        cout << "-1" << endl;
+    }
+    return 0;
+}
